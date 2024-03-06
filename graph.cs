8c8eac5e0ed9aea522b14296d6f7213f5636b134
drstrangelove4 <158036236+drstrangelove4@users.noreply.github.com>
2024-03-06 22:44:28 +0000

    Update graph.cs
    -Fixed the graph so it makes the graph like a traditional one instead of making X = 0 and Y = 0 be a full row with cells. 
    
    - It will print the X Instead of printing the column/row header if X = 0 or Y = 0. This is relevant if this used in other programs (although having a X appearing as a hardcoded thing should probably be moved into its own method call.

--- graph.cs
+++ graph.cs
@@ -1,8 +1,8 @@
 class Graph
 {
-    const int START_ROWS = 10;
-    const int END_ROWS = 2;
-    const int ROW_SIZE = 11;
+    const int START_ROWS = 10; // Will make X - 1 rows 
+    const int END_ROWS = 2; // must always be 2 to make the bottom 2 rows correctly.
+    const int ROW_SIZE = 11; // Will make X - 1 columns.
 
     List<List<string>> graph = [];    
 
@@ -11,23 +11,29 @@ class Graph
 
 
     public void Populate(int x, int y)
-        // A method that is responsible for creating all the graph "cells"
+    // A method that is responsible for creating all the graph "cells"
     {
         // Reset the contents of the graph - this will ensure we don't get repeat entries.
         graph.Clear();
 
-        // Populate the graph
-        for (int i = START_ROWS; i > -1; i--)           
+        // Populate the graph - we stop at 0, not - 1 as we dont want to draw x = 0 as its own row. 
+        for (int i = START_ROWS; i > 0; i--)
         {
             // Create the initial list for each row
             List<string> row = [];
-            
-            // Add the initial entry 
-            if (i == 10)
+
+            // Add the initial entry. Add the X instead of a number if we are aiming for X = 0 
+            if (x == 0 && i == y)
+            {
+                row.Add("  X");
+            }
+            // Remove the spacing from the X if number has 2 digits.
+            else if (i >= 10)
             {
                 row.Add($"{i}|");
             }
-            else
+            // Add spacing if we are dealing with single digit numbers.
+            else if (i < 10)
             {
                 row.Add($"{i} |");
             }
@@ -35,7 +41,7 @@ class Graph
             // Add the "Cells" to the row
             for (int j = 0; j < START_ROWS; j++)
             {
-                if (i == y && j == x)
+                if (i == y && j == x && x > 0)
                 {
                     row.Add("X ");
                 }
@@ -59,7 +65,16 @@ class Graph
             {
                 if (j == 0)
                 {
-                    row.Add("--");
+                    // Draw Y = 0
+                    if (y == 0 && i == x)
+                    {
+                        row.Add("X-");
+                    }
+                    // Else just draw a line.
+                    else
+                    {
+                        row.Add("--");
+                    }
                 }
                 else
                 {
@@ -67,7 +82,7 @@ class Graph
                 }
             }
             // Add the row to our class variable
-            graph.Add(row);            
+            graph.Add(row);
         }
     }
 
@@ -76,17 +91,25 @@ class Graph
 
 
     public void Print_graph()
-        // A method that prints the graph to the user
+    // A method that prints the graph to the user
     {
         foreach (List<string> row in graph)
         {
             Console.WriteLine();
             foreach (string cell in row)
             {
-                // Print Each cell and ensure the color is white (we change it in other parts of the program).
-                Console.Write(cell, Console.ForegroundColor = ConsoleColor.White);
+                // Highlight the X on the grid. 
+                if (cell == "X " || cell == "X-" || cell == "  X")
+                {
+                    Console.Write(cell, Console.ForegroundColor = ConsoleColor.Red);
+                }
+                else
+                {
+                    // Print Each cell and ensure the color is white.
+                    Console.Write(cell, Console.ForegroundColor = ConsoleColor.White);
+                }
             }
-        } 
+        }
         Console.WriteLine();
         Console.WriteLine();
     }
